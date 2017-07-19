729aa93c6b91f9dd26fc9f5154c9ce85318f63dd
Daniel Raineri <daniel.raineri@qa.com>
2017-07-19 15:39:11 +0100

    Fixed after reviews.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,14 +5,13 @@ Created by DRain on 18/07/2017.
 class Main {
   val input = "foobar\nwizard\ngsrh rh zm vcznkov lu gsv zgyzhs xrksvi"
   val cipher: Map[Char, Char] = (('a' to 'z').toList zip ('a' to 'z').toList.reverse).toMap
-  val zero = 0
 
   def encodeString(input: String): String = {
     val inputArray = input.toLowerCase.toCharArray
-    val output = for (i <- zero until inputArray.length) yield {
+    val output = for (i <- inputArray.indices) yield {
       i match {
         case _ if cipher.contains(inputArray(i)) => cipher(inputArray(i))
-        case _ if !cipher.contains(inputArray(i)) => inputArray(i)
+        case _ => inputArray(i)
       }
     }
     output.mkString
