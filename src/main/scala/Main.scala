704a1b3c9a7992f1fabfe28259c29c1b392f8fa6
Daniel Raineri <daniel.raineri@qa.com>
2017-07-19 10:57:04 +0100

    Initial

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,10 @@
+/*
+Created by DRain on 18/07/2017.
+*/
+
+import scala.util.{Failure, Success, Try}
+
+object Main extends App {
+
+
+}
---
+++ src/test/scala/Tests.scala
@@ -0,0 +1,11 @@
+/*
+Created by DRain on 18/07/2017.
+*/
+
+import org.scalatest._
+import Main._
+
+class Tests extends FlatSpec with Matchers {
+
+
+}
