4c40460367131acaf87f2b3ba690b853c6687818
Daniel Raineri <daniel.raineri@qa.com>
2017-07-19 14:31:41 +0100

    fixed hard coded mapping

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,17 +2,13 @@
 Created by DRain on 18/07/2017.
 */
 
-import scala.util.{Failure, Success, Try}
-
 class Main {
   val input = "foobar\nwizard\ngsrh rh zm vcznkov lu gsv zgyzhs xrksvi"
-  val cipher = Map("a" -> "z", "b" -> "y", "c" -> "x", "d" -> "w", "e" -> "v", "f" -> "u", "g" -> "t", "h" -> "s",
-    "i" -> "r", "j" -> "q", "k" -> "p", "l" -> "o", "m" -> "n", "n" -> "m", "o" -> "l", "p" -> "k", "q" -> "j",
-    "r" -> "i", "s" -> "h", "t" -> "g", "u" -> "f", "v" -> "e", "w" -> "d", "x" -> "c", "y" -> "b", "z" -> "a")
+  val cipher: Map[Char, Char] = (('a' to 'z').toList zip ('a' to 'z').toList.reverse).toMap
+  val zero = 0
 
   def encodeString(input: String): String = {
-    val zero = 0
-    val inputArray = input.toLowerCase.split("")
+    val inputArray = input.toLowerCase.toCharArray
     val output = for (i <- zero until inputArray.length) yield {
       i match {
         case _ if cipher.contains(inputArray(i)) => cipher(inputArray(i))
@@ -21,5 +17,6 @@ class Main {
     }
     output.mkString
   }
+
   print(encodeString(input))
 }
--- src/test/scala/Tests.scala
+++ src/test/scala/Tests.scala
@@ -4,9 +4,8 @@ Created by DRain on 18/07/2017.
 
 import org.scalatest._
 
-
 class Tests extends FlatSpec with Matchers {
-val main = new Main
+  val main = new Main
 
   "The given example1 input" should "return the correct string" in {
     val input = "foobar\nwizard\ngsrh rh zm vcznkov lu gsv zgyzhs xrksvi"
@@ -22,6 +21,5 @@ val main = new Main
     val input = "wizard!"
     main.encodeString(input) shouldBe "draziw!"
   }
-
 
 }
